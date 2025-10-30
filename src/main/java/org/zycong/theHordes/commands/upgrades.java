e864733388044eb5fce19a2bc25db5d2474ae4c2
zycong <lmdonders@outlook.com>
2025-10-30 13:48:33 +0100

    Update upgrades.java

--- src/main/java/org/zycong/theHordes/commands/upgrades.java
+++ src/main/java/org/zycong/theHordes/commands/upgrades.java
@@ -7,17 +7,37 @@ public class updates implements command {
 
     switch (args[1]){
       case ("edit") : {
-        createEditor(p, args[0]);
+        openEditor(p, args[0]);
       }
     }
   }
 
-  void createEditor(Player p, String kit){
+  void openEditor(Player p, String kit){
     Inventory inv = Bukkit.CreateInventory(p, 27, "Upgrade editor of " + kit);
-
-    
-    
-
+    List<String> upgradeOptions = (List)yamlManager.getInstance().getNodes("kits", kit + ".upgrades");
+    for (String s : upgradeOptions){
+      inv.addItem((ItemStack)yamlManager.getInstance().getOption("kit", kit + ".upgrades." + s + ".item"));
+    }    
     p.openInventory(inv);
+    p.setMetaData("inventory", new FixedMetaData(THeHordes.getPlugin(), "upgradeEditor"));
+    p.setMetaData("kit", new FixedMetaData(TheHordes.getPlugin(), kit))
+  }
+
+  public void editorUsed(InventoryClickEvent e){
+    if (e.getCurrentItem().getType().equals(Material.AIR)){
+      return; //later add drag to add new upgrades
+      e.setCanceled(true);
+    }
+    List<String> upgradeOptions = (List)yamlManager.getInstance().getNodes("kits", kit + ".upgrades");
+    String kit = e.getWhoClicked().getMetaData("kit").get(0).asString();
+    for (String s : upgradeOptions){
+      if ((ItemStack)yamlManager.getInstance().getOption("kit", kit + ".upgrades." + s + ".item").equals(e.getCurrentItem())){
+        createEditorForItem(e.getWhoClicked(), kit);
+      }
+    }
+  }
+
+  public void createEditorForItem(Player p, String kit){
+    
   }
 }
